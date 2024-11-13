8f3b8f3d3741c8dbd1c5dbb2acc7217dba2fcc7d
agtyra <keziagtyra@gmail.com>
2024-11-13 12:26:00 +0700

    added tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
 import 'package:mental_health_tracker/screens/menu.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
 
@@ -51,17 +52,16 @@ class LeftDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: const Icon(Icons.mood),
-            title: const Text('Add Mood'),
-            // Redirection part to MoodEntryFormPage
-            onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MoodEntryFormPage(),
-                  ));
-            },
-          )
+              leading: const Icon(Icons.add_reaction_rounded),
+              title: const Text('Mood List'),
+              onTap: () {
+                  // Route to the mood page
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+                  );
+              },
+          ),
         ],
       ),
     );
